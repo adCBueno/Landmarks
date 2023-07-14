d8a7fe090909e0aca28673d590dce6bdd60af1d9
adCBueno <adrianacc.bueno@gmail.com>
2023-07-14 08:21:34 -0400

    Create a Landmark Model

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -10,8 +10,7 @@ import MapKit
 
 /*
  Create a private state variable that holds the region information for the map.
- You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
- 
+ You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value. 
  */
 
 struct MapView: View {
@@ -23,7 +22,14 @@ struct MapView: View {
         /*
          By prefixing a state variable with $, you pass a binding, which is like a reference to the underlying value. When the user interacts with the map, the map updates the region value to match the part of the map that’s currently visible in the user interface.
          */
-        Map(coordinateRegion: $region)
+        VStack {
+            Button("Action") {
+                region = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+                    span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+            }
+            Map(coordinateRegion: $region)
+        }
     }
 }
 

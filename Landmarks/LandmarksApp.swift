f049d0129f432e77548c16fce5c4254caeb6d4f8
adCBueno <adrianacc.bueno@gmail.com>
2023-07-14 11:47:31 -0400

    Adopt the Model Object in Your Views
    Now that you’ve created the ModelData object, you need to update your views to adopt it as the data store for your app.

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -10,13 +10,15 @@ import SwiftUI
 // The @main attribute identifies the app’s entry point.
 @main
 struct LandmarksApp: App {
-    let persistenceController = PersistenceController.shared
+    // Use the @StateObject attribute to initialize a model object for a given property only once during the life time of the app. This is true when you use the attribute in an app instance, as shown here, as well as when you use it in a view.
+    @StateObject private var modelData = ModelData()
 
     //The structure’s body property returns one or more scenes, which in turn provide content for display.
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(modelData)
         }
     }
 }
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    // The modelData property gets its value automatically, as long as the environmentObject(_:) modifier has been applied to a parent.
+    @EnvironmentObject var modelData: ModelData
     
     @State private var showFavoritesOnly = false
     
      // Compute a filtered version of the landmarks list by checking the showFavoritesOnly property and each landmark.isFavorite value.
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
@@ -46,6 +48,7 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
         /*ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
